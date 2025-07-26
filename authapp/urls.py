24850582212f995ff3dbc0f087a45165490f5345
Aarupa <aartilahane013@gmail.com>
2025-07-26 12:24:30 +0530

    remove websocket and poly

--- authapp/urls.py
+++ authapp/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('gmtt-chat/', ChatbotAPI.as_view(), name='gmtt_chat'),
     path('interview/', InterviewBotAPI.as_view(), name='interview_receiver'),
     path('chat-history/', ChatHistoryAPI.as_view(), name='chat_history'),
-    path('nirankari-chat/', ChatbotAPI.as_view(), name='nirankari_chat'),
+    # path('nirankari-chat/', ChatbotAPI.as_view(), name='nirankari_chat'),
 
     path('forgot-password/', ForgotPasswordAPI.as_view(), name='forgot-password'),
     path('reset-password/<uidb64>/<token>/', ResetPasswordAPI.as_view(), name='reset-password'),
